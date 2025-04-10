2145c37bd3789a5686f7a66e652b6b6e1cf5d7e0
Ndzalama Mabasa <Ndzalama.Mabasa@bbd.co.za>
2025-04-10 06:01:57 +0200

    add service layer for the planet model

---
+++ api/Services/PlanetService.cs
@@ -0,0 +1,14 @@
+using galaxy_match_make.Models;
+using galaxy_match_make.Repositories;
+
+namespace galaxy_match_make.Services;
+
+public class PlanetService : IPlanetService
+{
+    private readonly IPlanetRepository _planetRepository;
+    public PlanetService(IPlanetRepository planetRepository) => _planetRepository = planetRepository;
+    public async Task<IEnumerable<PlanetDto>> GetAllPlanetsAsync()
+    {
+        return await _planetRepository.GetAllPlanetsAsync();
+    }
+}
